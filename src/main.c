fd4f41eaefc2b9f234217f0d78b0ed49551d4ae0
YiyangFan <18615302908@163.com>
2024-09-13 12:46:52 +0000

    teat

--- src/main.c
+++ src/main.c
@@ -17,12 +17,9 @@ void main() {
         uart_init();
         printk_init();
 
-        /* @todo: Print "Hello, world! (Core 0)" */
+        /* initialize kernel memory allocator */
+        kinit();
 
-        const char *message = "Hello, world! (Core 0)\n";
-        for (int i = 0; message[i]!= '\0'; i++) {
-            uart_put_char(message[i]);
-        }
         arch_fence();
 
         // Set a flag indicating that the secondary CPUs can start executing.
@@ -30,15 +27,6 @@ void main() {
     } else {
         while (!boot_secondary_cpus);
         arch_fence();
-
-        /* @todo: Print "Hello, world! (Core <core id>)" */
-        const char *message = "Hello, world! (Core ";
-        for (int i = 0; message[i]!= '\0'; i++) {
-            uart_put_char(message[i]);
-        }
-        uart_put_char((char)('0' + cpuid())); // 将核心编号转换为对应的字符并输出
-        uart_put_char(')');
-        uart_put_char('\n');
     }
 
     set_return_addr(idle_entry);
